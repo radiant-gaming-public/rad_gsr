daed4fde03f9cc2a55a876c40f515aac57e91841
BattleRatttt <39086978+BattleRattt@users.noreply.github.com>
2019-07-29 16:00:16 -0500

    Added Character Name In Notify Option

--- config.lua
+++ config.lua
@@ -7,3 +7,4 @@ Config.waterCleanTime           = 30 * 1000         -- Change first number only,
 Config.gsrTime                  = 30 * 60           -- Change The first number only, if you want the GSR to be auto removed faster output is minutes (default: 30 min).
 Config.gsrAutoRemove            = 10 * 60 * 1000    -- Change first number only, to set the auto clean up in minuets (default: 10 min).
 Config.gsrUpdateStatus          = 5 * 60 * 1000     -- Change first number only, to change how often the client updates hasFired variable dont set it to high 5-10 min should be fine. (default: 5 min).
+Config.UseCharName				= true				-- This will show the suspects name in the PASSED or FAILED notification.Allows cop to make sure they checked the right person.
