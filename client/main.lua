0c4f79031afe0109d07e231b7df932e2694ba557
BattleRatttt <39086978+BattleRattt@users.noreply.github.com>
2019-11-14 15:55:18 -0600

    Added Weapon Checklist

--- client/main.lua
+++ client/main.lua
@@ -1,5 +1,6 @@
 ESX = nil
-local hasShot = false
+local hasShot        = false
+local ignoreShooting = false
 
 Citizen.CreateThread(function()
     while ESX == nil do
@@ -18,9 +19,19 @@ Citizen.CreateThread(function()
         Citizen.Wait(0)
         ped = PlayerPedId()
         if IsPedShooting(ped) then
-            TriggerServerEvent('GSR:SetGSR', timer)
-            hasShot = true
-            Citizen.Wait(Config.gsrUpdate)
+            local currentWeapon = GetSelectedPedWeapon(ped)
+            for _,k in pairs(Config.weaponChecklist) do
+                if currentWeapon == k then
+                    ignoreShooting = true
+                    break
+                end
+            end
+            if not ignoreShooting then
+                TriggerServerEvent('GSR:SetGSR', timer)
+                hasShot = true
+                ignoreShooting = false
+                Citizen.Wait(Config.gsrUpdate)
+            end
         end
     end
 end)
@@ -31,32 +42,15 @@ Citizen.CreateThread(function()
         if Config.waterClean and hasShot then
             ped = PlayerPedId()
             if IsEntityInWater(ped) then
-				exports['mythic_notify']:DoCustomHudText('inform', 'You begin cleaning off the Gunshot Residue... stay in the water', 5000)
-				Wait(100)
-				TriggerEvent("mythic_progbar:client:progress", {
-        			name = "washing_gsr",
-        			duration = Config.waterCleanTime,
-        			label = "Washing Off GSR",
-        			useWhileDead = false,
-        			canCancel = true,
-        			controlDisables = {
-            			disableMovement = false,
-            			disableCarMovement = false,
-            			disableMouse = false,
-            			disableCombat = false,
-        			},
-    			}, function(status)
-        			if not status then
-            			if IsEntityInWater(ped) then
-                    		hasShot = false
-                    		TriggerServerEvent('GSR:Remove')
-							exports['mythic_notify']:DoCustomHudText('success', 'You washed off all the Gunshot Residue in the water', 5000)
-                		else
-							exports['mythic_notify']:DoCustomHudText('error', 'You left the water too early and did not wash off the gunshot residue.', 5000)
-                		end
-        			end
-    			end)
-				Citizen.Wait(Config.waterCleanTime)
+                TriggerEvent("chatMessage", "[GSR]", {255, 0, 0}, "You begin cleaning off the Gunshot Residue... stay in the water")
+                Citizen.Wait(Config.waterCleanTime)
+                if IsEntityInWater(ped) then
+                    hasShot = false
+                    TriggerServerEvent('GSR:Remove')
+                    TriggerEvent("chatMessage", "[GSR]", {255, 0, 0}, "^2You washed off all the Gunshot Residue in the water")
+                else
+                    TriggerEvent("chatMessage", "[GSR]", {255, 0, 0}, "^1You left the water too early and did not wash off the gunshot residue.")
+                end
             end
         end
     end
